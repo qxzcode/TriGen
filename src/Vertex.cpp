eeb3050bd88fa3cf7dad764211afb633298c0af4
qxzcode <qxzcode@users.noreply.github.com>
2015-01-27 14:29:18 -0800

    Writing add/remove vert/tri functions of Vertex

--- src/Vertex.cpp
+++ src/Vertex.cpp
@@ -8,6 +8,8 @@
 
 #include "Vertex.h"
 
+#include "Triangle.h"
+
 Vertex::Vertex(vec3f pos):pos(pos),valence(0),aTris(NULL),aVerts(NULL) {
 	
 }
@@ -15,6 +17,13 @@ Vertex::Vertex(vec3f pos):pos(pos),valence(0),aTris(NULL),aVerts(NULL) {
 Vertex::~Vertex() {
 	delete[] aTris;
 	delete[] aVerts;
+}
+
+//Writing add/remove vert/tri functions of Vertex
+
+void Vertex::addTri(Triangle* t) {
+	Triangle** oldTris = aTris;
+	aTris = new Triangle*[++valence];
 }
 
 void Vertex::edgeTris(Vertex* v1, Vertex* v2, class Triangle** t1, Triangle** t2) {
