1174b936620fc6239158e7b0b35366879da80c97
qxzcode <qxzcode@users.noreply.github.com>
2015-01-27 14:23:19 -0800

    Writing add/remove vert/tri functions of Vertex

--- src/Vertex.h
+++ src/Vertex.h
@@ -10,14 +10,20 @@
 
 #include "vec3.h"
 
+class Triangle;
+
 class Vertex {
 public:
 	Vertex(vec3f pos);
 	~Vertex();
 	
 	// functions for manipulation and stuff
+	void addTri(Triangle* t);
+	void removeTri(Triangle* t);
+	void addVert(Vertex* v);
+	void removeVert(Vertex* v);
 	bool onUpdate(class Mesh* mesh);
-	static void edgeTris(Vertex* v1, Vertex* v2, class Triangle** t1, Triangle** t2);
+	static void edgeTris(Vertex* v1, Vertex* v2, Triangle** t1, Triangle** t2);
 	
 	vec3f pos;
 	unsigned char valence;
